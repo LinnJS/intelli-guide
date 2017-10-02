5ad0413e67ab1bc8bbdd0acdaf32dbb146e40c75
JSLinn727 <justinslinn727@gmail.com>
2017-10-02 18:38:48 -0400

    Added Header component

---
+++ src/components/IndexHeader/IndexHeader.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import './IndexHeader.css'
+// import head from './daliHead.svg'
+
+class IndexHeader extends Component {
+  render() {
+    return (
+      <div className="IndexHeader">
+        <i className="fa fa-cog fa-lg" aria-hidden="true" />
+        <nav>
+          <i className="fa fa-language fa-lg" aria-hidden="true" />
+          <i className="fa fa-star-o fa-lg" aria-hidden="true" />
+          <i className="fa fa-map-o fa-lg" aria-hidden="true" />
+          <i className="fa fa-search fa-lg" aria-hidden="true" />
+        </nav>
+        {/* <img src={head} alt="" /> */}
+      </div>
+    )
+  }
+}
+
+export default IndexHeader
