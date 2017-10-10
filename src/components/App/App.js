9bd84f2630aa32690bc5d0e23b0d15f2506341d5
wwwhatley <30512540+wwwhatley@users.noreply.github.com>
2017-10-10 17:19:05 -0400

    Created SelectionView component, started working on styling.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import IndexSelection from '../IndexSelection/IndexSelection';
-import ListView from '../ListView/ListView';
+// import ListView from '../ListView/ListView';
+import SelectionView from '../SelectionView/SelectionView'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         {/* <IndexSelection /> */}
-        <ListView />
+        {/* <ListView /> */}
+        <SelectionView />
       </div>
     );
   }
--- src/components/ListView/ListView.jsx
+++ src/components/ListView/ListView.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import style from './ListView.css';
 import InternalHeader from '../InternalHeader/InternalHeader';
-import daliFooter from '../../assets/daliFooter.svg';
 import daddyLongLegs from '../../assets/daddyLongLegs.jpg';
 import guideIcon from '../../assets/guide.png';
 
