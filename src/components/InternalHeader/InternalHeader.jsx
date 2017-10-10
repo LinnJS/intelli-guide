c58ab8477c03c752b0fd167be5549b0df4d8c955
JSLinn727 <justinslinn727@gmail.com>
2017-10-10 16:44:21 -0400

    Working back button

--- src/components/InternalHeader/InternalHeader.jsx
+++ src/components/InternalHeader/InternalHeader.jsx
@@ -8,13 +8,17 @@ class IndexHeader extends Component {
   render() {
     return (
       <div className="IndexHeader">
-        <i class="fa fa-arrow-left fa-lg" aria-hidden="true" />
-        <nav>
-          <i className="fa fa-language f-lg " aria-hidden="true" />
-          <i className="fa fa-star-o fa-lg" aria-hidden="true" />
-          <i className="fa fa-map-o fa-lg" aria-hidden="true" />
-          <i className="fa fa-search fa-lg" aria-hidden="true" />
-        </nav>
+        <Switch>
+          <Link to="/">
+            <i class="fa fa-arrow-left fa-lg" aria-hidden="true" />
+          </Link>
+          <nav>
+            <i className="fa fa-language f-lg " aria-hidden="true" />
+            <i className="fa fa-star-o fa-lg" aria-hidden="true" />
+            <i className="fa fa-map-o fa-lg" aria-hidden="true" />
+            <i className="fa fa-search fa-lg" aria-hidden="true" />
+          </nav>
+        </Switch>
       </div>
     );
   }
