47b445d47f41845bc9a5c055c62d318cf2b5ee15
JSLinn727 <justinslinn727@gmail.com>
2017-10-10 14:11:20 -0400

    Added react-router-dom and added <Link /> evlements with to'' placeholder

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
 import './App.css';
 import IndexSelection from '../IndexSelection/IndexSelection';
 import ListView from '../ListView/ListView';
@@ -6,10 +8,12 @@ import ListView from '../ListView/ListView';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        {/* <IndexSelection /> */}
-        <ListView />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <IndexSelection />
+          {/* <ListView /> */}
+        </div>
+      </BrowserRouter>
     );
   }
 }
--- src/components/IndexSelection/IndexSelection.jsx
+++ src/components/IndexSelection/IndexSelection.jsx
@@ -1,7 +1,10 @@
-import React, { Component } from 'react'
-import styles from './IndexSelection.css'
-import IndexHeader from '../IndexHeader/IndexHeader'
-import daliFooter from '../../assets/daliFooter.svg'
+import React, { Component } from 'react';
+import { Switch, Link } from 'react-router-dom';
+
+import styles from './IndexSelection.css';
+import IndexHeader from '../IndexHeader/IndexHeader';
+import daliFooter from '../../assets/daliFooter.svg';
+import ListView from '../ListView/ListView';
 
 class IndexSelection extends Component {
   render() {
@@ -10,36 +13,44 @@ class IndexSelection extends Component {
         <IndexHeader />
         <div className="wrapper">
           <section className="permanent">
-            <div className="indexText">
-              <span>permanent collection tour</span>
-              <h3>the works of dali</h3>
-            </div>
+            <Link to="ListView">
+              <div className="indexText">
+                <span>permanent collection tour</span>
+                <h3>the works of dali</h3>
+              </div>
+            </Link>
           </section>
           <section className="expedition">
-            <div className="indexText">
-              <span>expedition tour</span>
-              <h3>expedition tour</h3>
-            </div>
+            <Link to="ListView">
+              <div className="indexText">
+                <span>expedition tour</span>
+                <h3>expedition tour</h3>
+              </div>
+            </Link>
           </section>
           <section className="architecture">
-            <div className="indexText">
-              <span>understanding the enigma</span>
-              <h3>architecture tour</h3>
-            </div>
+            <Link to="ListView">
+              <div className="indexText">
+                <span>understanding the enigma</span>
+                <h3>architecture tour</h3>
+              </div>
+            </Link>
           </section>
           <section className="family">
-            <div className="indexText">
-              <span>decode the imagination</span>
-              <h3>family tours</h3>
-            </div>
+            <Link to="ListView">
+              <div className="indexText">
+                <span>decode the imagination</span>
+                <h3>family tours</h3>
+              </div>
+            </Link>
           </section>
         </div>
         <footer>
           <img src={daliFooter} alt="Dali Footer Branding" />
         </footer>
       </div>
-    )
+    );
   }
 }
 
-export default IndexSelection
+export default IndexSelection;
