4543ddb6ad4b1b5783bd2e9e57c733d4f3f97b66
JSLinn727 <justinslinn727@gmail.com>
2017-10-12 16:30:06 -0400

    Ham menu package added

--- src/components/IndexHeader/IndexHeader.jsx
+++ src/components/IndexHeader/IndexHeader.jsx
@@ -1,12 +1,22 @@
-import React, { Component } from 'react'
-import './IndexHeader.css'
+import React, { Component } from 'react';
+import { slide as Menu } from 'react-burger-menu';
+
+import './IndexHeader.css';
 // import head from './daliHead.svg'
 
 class IndexHeader extends Component {
+  showSettings(event) {
+    event.preventDefault();
+  }
+
   render() {
     return (
       <div className="IndexHeader">
-        <i className="fa fa-cog fa-lg" aria-hidden="true" />
+        <div>
+          {/* <Menu /> */}
+          <i className="fa fa-cog fa-lg" aria-hidden="true" />
+        </div>
+        <a onClick={this.showSettings} className="menu-item--small" href="" />
         <nav>
           <i className="fa fa-language fa-lg " aria-hidden="true" />
           <i className="fa fa-star-o fa-lg" aria-hidden="true" />
@@ -14,8 +24,8 @@ class IndexHeader extends Component {
           <i className="fa fa-search fa-lg" aria-hidden="true" />
         </nav>
       </div>
-    )
+    );
   }
 }
 
-export default IndexHeader
+export default IndexHeader;
